c2d1f67b63290ae9da1ccf2b3b6ce0c289720895
Murphy <sydneyporter74@yahoo.com>
2020-12-20 20:11:55 -0500

    made adjustments to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,21 +5,80 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     "/",
-    "./index.html",
-    "./js/index.js",
-    "./js/idb.js",
-    "./css/styles.css",
+    "/index.html",
+    "/js/index.js",
+    "/js/idb.js",
+    "/css/styles.css",
+    "/manifest.json",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    
 ];
 
 
 // install the service worker
 self.addEventListener('install', function (e) {
+    console.log("Installing Service Worker");
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
-    )
+    );
+});
+
+
+// tells the app which information to retrieve from the cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches
+                .open(CACHE_NAME)
+                .then(cache => {
+                    return fetch(e.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(e.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(e.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+    e.respondWith(
+        fetch(e.request).catch(function() {
+            return caches.match(e.request).then(function(response) {
+                if (response) {
+                    return response;
+                }
+                else if (e.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            });
+        })
+    );       
+    
+        // determines if the resource already exists in caches
+        // caches.match(e.request).then(function (request) {
+        //     if (request) {
+        //         console.log('responding with cache: ' + e.request.url)
+        //         return request
+        //     }
+        //     // if not we allow the resource to be retrieved using online network
+        //     else {
+        //         console.log('file is not cached, fetching : ' + e.request.url)
+        //         return fetch(e.request)
+        //     }
+        // })
+        
+    
 });
 
 
@@ -42,25 +101,8 @@ self.addEventListener('activate', function (e) {
             );
         })
     );
+
+    self.clients.claim();
 });
 
 
-// tells the app which information to retrieve from the cache
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        // determines if the resource already exists in caches
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache: ' + e.request.url)
-                return request
-            }
-            // if not we allow the resource to be retrieved using online network
-            else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-        
-    )
-})
